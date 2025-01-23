7d0cbea64cccad21be8f0b682b1c52ab9be247f3
Irfan <amonium7@gmail.com>
2025-01-23 13:06:33 +0100

    ejs

--- ejsapp/routes/index.js
+++ ejsapp/routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var path = require('path');
+router.get("/:newUser", function (req, res, next) {
+  res.render("index", { newUser: req.params.newUser });
+});
+router.use(express.static(path.join(__dirname, '..')));
 router.get('/', function(req, res, next) {
+  // res.sendFile(path.join(__dirname, '..', 'index.html'));
   res.render('index', { title: 'Express' });
 });
 
